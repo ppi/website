e0417d809b91039e90749aaa5f6d5c0ab0ed93ad
Ben Tadiar <ben@bentadiar.co.uk>
2011-10-26 21:39:21 +0100

    Added .htaccess and fixed contributors.js not preventing click event

--- scripts/contributors.js
+++ scripts/contributors.js
@@ -1,21 +1,23 @@
 jQuery(document).ready(function() {
 
-	$('.contributors-item .links a').click( function() {
+	$('.contributors-item .links a').click( function(event) {
+		event.preventDefault();
 		$(this).parents('.contributors-item').find('.committers').slideToggle();
-
-        if ($(this).text() == "Show Contributors") {
-            $(this).html("Hide Contributors");
-        } else {
-            $(this).html("Show Contributors");
-        }
-
-        if ($(this).text() == "Show Members") {
-            $(this).html("Hide Members");
-        } else {
-            $(this).html("Show Members");
-        }
-
-		return false;
+		
+		var $text = $(this).text();
+		
+		if($text === 'Show Contributors'){
+			$(this).text('Hide Contributors');
+		} else if($text === 'Hide Contributors'){
+			$(this).text('Show Contributors');
+		}
+		
+		if($text === 'Show Members'){
+			$(this).text('Hide Members');
+		} else if($text === 'Hide Members'){
+			$(this).text('Show Members');
+		}
+		
 	});
 
 	$('.contributors-item .links a:first').trigger('click');
