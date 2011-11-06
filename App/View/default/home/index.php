69f49802b7b3945af9c8227eb5e08bb5c29dff71
Paul Dragoonis <dragoonis@gmail.com>
2011-11-06 18:36:56 +0000

    Updating docs links and adding short_tags options.

--- App/View/default/home/index.php
+++ App/View/default/home/index.php
@@ -10,7 +10,7 @@
 					<div class="download item">
 						<a href="<?= $baseUrl; ?>downloads" title="Downloads"><img src="images/icons-download.png" alt="download"></a>
 						<div class="item-content">
-							<h3><a href="<?php echo $baseUrl; ?>downloads">downloads</a></h3>
+							<h3><a href="<?= $baseUrl; ?>downloads">downloads</a></h3>
 							<p>The downloads section consists of all the PPI related projects that we have hosted on github. We have projects such as the framework, skeleton app and the issue tracker. We also host the repositories we use for this website and our documentation. Everything is open source!</p>
 						</div>
 					</div>
@@ -26,7 +26,7 @@
 					<div class="documentation item">
 						<a href="<?= $baseUrl; ?>docs" title=""><img src="<?= $baseUrl; ?>images/icons-documentation.png" alt="documentation"></a>
 						<div class="item-content">
-							<h3><a href="<?php echo $baseUrl; ?>docs">documentation</a></h3>
+							<h3><a href="http://docs.ppi.io">documentation</a></h3>
 							<p>Tutorials are the best way to get started with PPI. It explains everything you need to know; How to wield its many features to develop web applications faster and more efficiently, even if you only know a little bit of PHP.</p>
 						</div>
 					</div>
@@ -35,12 +35,12 @@
 					<div class="development item">
 						<a href="<?php echo $baseUrl; ?>community" title="Development"><img src="<?php echo $baseUrl; ?>images/icons-development.png" alt="development"></a>
 						<div class="item-content">
-							<h3><a href="<?php echo $baseUrl; ?>support" title="Development">development</a></h3>
+							<h3><a href="<?= $baseUrl; ?>support" title="Development">development</a></h3>
 							<p>Want to get involved in the project? We're looking for designers, front-end developers and back-end developers. We will always have something to suit your mood. You can pick up a ticket on our <a href="<?php echo $baseUrl; ?>support" title="Issue Tracker">Issue Tracker</a> and get stuck in.</p>
 						</div>
 					</div>
 					<div class="github item">
-						<a href="http://github.com/ppi"><img src="<?php echo $baseUrl; ?>images/icons-github.png" alt="github"></a>
+						<a href="http://github.com/ppi"><img src="<?= $baseUrl; ?>images/icons-github.png" alt="github"></a>
 						<div class="item-content">
 							<h3><a href="http://github.com/ppi" target="_blank" title="github">github</a></h3>
 							<p>Clone one of our projects, create a <a href="http://help.github.com/forking" target="_blank" title="fork">fork</a>, submit a <a href="http://help.github.com/pull-requests" target="_blank" title="pull request">pull request</a>, that's how we do it in github-land. Whether you're looking to just grab the latest version of the code, <a href="https://github.com/dragoonis/PPI-Framework/watchers" target="_blank" title="">follow the github project</a> or have a snoop about, you can find it all <a href="https://github.com/dragoonis" target="_blank" title="github">here</a>.</p>
