65613fb7f732e7dd455b9070f96d45995c99e011
Paul Dragoonis <dragoonis@gmail.com>
2011-11-21 12:21:15 +0000

    Docs modifications, styling and code example structures

--- App/View/default/docs/controller.php
+++ App/View/default/docs/controller.php
@@ -1,16 +1,28 @@
-<div class="content-box">
+<article class="content-box docs-page">
+
+	<aside class="toc">
+		<p class="heading">Table of Contents</p>
+		<ul class="items">
+			<li><a href="#" title="">Making your controller</a></li>
+			<li><a href="#" title="">Accessing your controller</a></li>
+			<li><a href="#" title="">Controllers with arguments</a></li>
+			<li><a href="#" title="">Loading a view</a></li>
+			<li><a href="#" title="">Loading an Ajax View</a></li>
+			<li><a href="#" title="">Handling Forms</a></li>
+			<li><a href="#" title="">Passing data to a view</a></li>
+			<li><a href="#" title="">Defining the default controller</a></li>
+		</ul>
+	</aside>
+
 	<h1>Using Controllers</h1>
 	<h1 id="anchor-introduction">Introduction</h1>
 	<p>Controllers are simply classes that are named in a way that they can be associated with a URI.</p>
 	<p>They are in charge of controlling flow and contain the bussiness logic for your application. They retrieve information via various methods such as from the database using Models. They then pass that information onto the presentation layer (A View) to be displayed.</p>
 	<br>
 	<p>The following documentation will outline the various ways in which you can interact with PPI's controller layer.</p>
-</div>
 
-<div class="content-box">
 	<h1 id="anchor-making-your-controller">Making your controller</h1>
-	<pre><code class="php">
-&lt;?php
+	<pre><code class="php">&lt;?php
 // File: App/Controller/Home.php
 namespace App\Controller;
 class \App\Controller\Home extends \App\Contrlller\Application {
@@ -21,9 +33,7 @@ class \App\Controller\Home extends \App\Contrlller\Application {
 
 }
 	</code></pre>
-</div>
 
-<div class="content-box">
 	<h1 id="anchor-accessing-your-controller">Accessing your controller</h1>
 	<p>As mentioned in the introduction, we associate a URI with a contoller. This is how we do it.</p>
 	<pre><code>
@@ -31,8 +41,7 @@ http://hostname/controller/method
 	</code></pre>
 	<p>The part "controller" matches the last segment of your class name. The part "method" is the method that is called on your controller. If you do not specify a method in your URI then the default method called is "index".</p>
 	<p>Consider the following:</p>
-	<pre><code class="php">
-&lt;?php
+	<pre><code class="php">&lt;?php
 // File: App/Controller/User.php
 namespace App\Controller;
 class \App\Controller\User extends \App\Contrlller\Application {
@@ -58,9 +67,7 @@ http://localhost/myapp/user/index
 	<pre><code>
 http://localhost/myapp/user/profile
 	</code></pre>
-</div>
 
-<div class="content-box">
 	<h1 id="anchor-controllers-with-arguments">Controllers with arguments</h1>
 	<p>To pass arguments aka "parameters" you can send the via the URI. Here is an example:</p>
 	<pre><code>
@@ -68,8 +75,7 @@ http://localhost/myapp/user/profile/23
 	</code></pre>
 
 	<p>To obtain the ID from the URI we can use the get() method.</p>
-	<pre><code class="php">
-&lt;?php
+	<pre><code class="php">&lt;?php
 namespace App\Controller;
 class \App\Controller\User extends \App\Contrlller\Application {
 
@@ -81,13 +87,9 @@ class \App\Controller\User extends \App\Contrlller\Application {
 }
 	</pre></code>
 
-</div>
-
-<div class="content-box">
 	<h1 id="anchor-loading-a-view">Loading a view</h1>
 	<p>We use the load() method to load up a view file, for the homepage, from your application's view folder.</p>
-	<pre><code class="php">
-namespace App\Controller;
+	<pre><code class="php">namespace App\Controller;
 class \App\Controller\Home extends \App\Contrlller\Application {
 
 	// Url: http://localhost/myapp/
@@ -97,15 +99,11 @@ class \App\Controller\Home extends \App\Contrlller\Application {
 }
 	</code></pre>
 
-</div>
-
-<div class="content-box">
 	<h1 id="anchor-loading-an-ajax-view">Loading an Ajax View</h1>
 	<p>Imagine you're making a twitter-like application where on a user's profile you can click "See John's Followers" and you want to get them via ajax.</p>
 	<p>The following is an example of loading an ajax view being loaded with the $isAjax variable set to true.</p>
 
-	<pre><code class="php">
-&lt;?php
+	<pre><code class="php">&lt;?php
 namespace App\Controller;
 class \App\Controller\User extends \App\Contrlller\Application {
 
@@ -119,13 +117,10 @@ class \App\Controller\User extends \App\Contrlller\Application {
 }
 
 	</code></pre>
-</div>
 
-<div class="content-box">
 	<h1 id="anchor-handling-forms">Handling Forms</h1>
 	<p>You can submit forms to controller methods and obtain the POST information using the post() method. Here we display a create user page and also handle the form submit in the create() method.</p>
-	<pre><code class="php">
-&lt;?php
+	<pre><code class="php">&lt;?php
 namespace App\Controller;
 class \App\Controller\User extends \App\Contrlller\Application {
 
@@ -148,14 +143,10 @@ class \App\Controller\User extends \App\Contrlller\Application {
 }
 	</code></pre>
 
-</div>
-
-<div class="content-box">
 	<h1 id="anchor-passing-data-to-a-view">Passing data to a view</h1>
 	<p>Passing data to a view is very simple. Below is an example where we take an argument using get(). Load up a model, get the user's information and pass that to the view for rendering.</p>
 
-	<pre><code class="php">
-&lt;?php
+	<pre><code class="php">&lt;?php
 namespace App\Controller;
 class \App\Controller\User extends \App\Contrlller\Application {
 
@@ -170,8 +161,7 @@ class \App\Controller\User extends \App\Contrlller\Application {
 }
 	</code></pre>
 
-<pre><code class="php">
-&lt;?php
+<pre><code class="php">&lt;?php
 namespace App\Controller;
 class \App\Controller\User extends \App\Contrlller\Application {
 
@@ -192,19 +182,13 @@ class \App\Controller\User extends \App\Contrlller\Application {
 }
 	</code></pre>
 
-</div>
-
-<div class="content-box">
-
 	<h1 id="anchor-defining-the-master-controller">Defining the default controller</h1>
 	<p>The "default controller" is maintained from your PPI application's routes file commonly located at App/Config/routes.php. A default controller is needed so that when you access the root of your website there will still be a controller dispatched. Here is an example:</p>
-	<pre><code class="ini">
-// File: App/Config/routes.php
+	<pre><code class="ini">// File: App/Config/routes.php
 $routes['__default__'] = 'home/index';
 	</code></pre>
 	<p>The following examples equate to the exact same thing. They will access the exact same method of "index" in the Home controller.</p>
-	<pre><code>
-http://localhost/myapp
+	<pre><code>http://localhost/myapp
 http://localhost/myapp/home
 http://localhost/myapp/home/index
 	</code></pre>
