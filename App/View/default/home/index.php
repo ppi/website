d7e01e76df1dbf2aad0809dbe2b7527e66b05456
Paul Dragoonis <dragoonis@gmail.com>
2011-11-21 11:59:10 +0000

    new icons and spacing on the homepage grid

--- App/View/default/home/index.php
+++ App/View/default/home/index.php
@@ -27,32 +27,33 @@
 	<p class="heading">Why use PPI?</p>
 	<ul class="list">
 		<li>
-			<a class="icon" href="" title=""><img src="<?= $baseUrl; ?>images/light/tools.png" alt="Icon"></a>
+			<a class="icon" href="" title=""><img src="<?= $baseUrl; ?>images/light/1day.png" alt="Icon"></a>
 			<a class="title" href="#" title="">Super Fast Development</a>
 			<p class="desc">Many common components are included: translation tools, database access (including PDO, MongoDB, CouchDB), Cache tools, many templating systems and much more.</p>
 		</li>
 		<li>
-			<a class="icon" href="" title="" style="margin-top: -18px;"><img src="<?= $baseUrl; ?>images/light/flexibility.png" alt="Icon"></a>
+			<a class="icon" href="" title=""><img src="<?= $baseUrl; ?>images/light/components.png" alt="Icon"></a>
 			<a class="title" href="#" title="">Flexibility</a>
 			<p class="desc">PPI is carefully designed to work well with third-party code. For example, you can use code from PEAR or Zend Framework, Doctrine or Symfony in your PPI application.</p>
 		</li>
 		<li style="margin-right: 0;">
-			<a class="icon" href="" title="" style="margin-top: -12px"><img src="<?= $baseUrl; ?>images/light/abacus.png" alt="Icon"></a>
+			<a class="icon" href="" title=""><img src="<?= $baseUrl; ?>images/light/easy
+			.png" alt="Icon"></a>
 			<a class="title" href="#" title="">Dev never was so easy</a>
 			<p class="desc">PPI helps you develop clean and reusable code. It follows the MVC pattern, ensuring a clear separation of logic and presentation.</p>
 		</li>
 		<li>
-			<a class="icon" href="" title="" style="margin-top: -12px;"><img src="<?= $baseUrl; ?>images/light/performance.png" alt="Icon"></a>
+			<a class="icon" href="" title=""><img src="<?= $baseUrl; ?>images/light/perf.png" alt="Icon"></a>
 			<a class="title" href="#" title="">Performance, first!</a>
 			<p class="desc">Benchmarking a framework is hard and rarely reflects real world usage, but PPI is very efficient and carefully optimized for real world usage.</p>
 		</li>
 		<li>
-			<a class="icon" href="" title="" style="margin-top: -12px;"><img src="<?= $baseUrl; ?>images/light/code.png" alt="Icon"></a>
+			<a class="icon" href="" title=""><img src="<?= $baseUrl; ?>images/light/config.png" alt="Icon"></a>
 			<a class="title" href="#" title="">No code generators</a>
 			<p class="desc">There are no code generators and endless configuration files, so setting up is fast an easy.</p>
 		</li>
 		<li style="margin-right: 0;">
-			<a class="icon" href="" title="" style="margin-top: -12px"><img src="<?= $baseUrl; ?>images/light/community.png" alt="Icon"></a>
+			<a class="icon" href="" title=""><img src="<?= $baseUrl; ?>images/light/community2.png" alt="Icon"></a>
 			<a class="title" href="#" title="">Our community rocks</a>
 			<p class="desc">Many common components are included: translation tools, database access (including PDO, MongoDB, CouchDB), Cache tools, many templating systems and much more.</p>
 		</li>
@@ -64,7 +65,7 @@
 	<p class="header">Community</p>
 	<ul class="list">
 		<li>
-			<a class="icon" href="" title="" style="margin-top: -12px;"><img src="<?= $baseUrl; ?>images/light/IRC.png" alt="Icon"></a>
+			<a class="icon" href="" title=""><img src="<?= $baseUrl; ?>images/light/chat.png" alt="Icon"></a>
 			<a class="title" href="<?= $baseUrl; ?>live-chat" title="IRC Live Chat">IRC</a>
 			<p class="desc">Chat with the ppi guys at #ppi on freenode.</p>
 		</li>
