ec076af8727c270add284a739d179f51f023a26d
Paul Dragoonis <dragoonis@gmail.com>
2011-11-21 10:07:30 +0000

    Font mods

--- App/Config/routes.php
+++ App/Config/routes.php
@@ -4,6 +4,8 @@ $routes = array();
 $routes['__404__'] = 'general/show404';
 $routes['__default__'] = 'home/index';
 
+$routes['/docs/(:any)\.html'] = 'docs/index/$1';
+
 // About Match
 $routes['/live-chat'] = 'community/livechat';
 $routes['/contributors'] = 'community/contributors';
