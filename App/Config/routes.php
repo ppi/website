572f539c502434c82c40853bfaed52209c637ae0
Alfredo Juarez <alfrekjv@gmail.com>
2011-11-27 19:26:56 -0600

    start contributing page - not finished yet

--- App/Config/routes.php
+++ App/Config/routes.php
@@ -9,3 +9,4 @@ $routes['/docs/(:any)\.html'] = 'docs/index/$1';
 // About Match
 $routes['/live-chat'] = 'community/livechat';
 $routes['/contributors'] = 'community/contributors';
+$routes['/contribute'] = 'community/contribute';
---
+++ App/View/default/community/contribute.php
@@ -0,0 +1,48 @@
+<div class="contribute-page">
+	<div class="left-side">
+		<nav>
+		<ul>
+			<li><a class="first" href="<?= $baseUrl; ?>contributors" target="_blank">Contributors</a></li>
+			<li><a class="first" href="http://www.github.com/ppi" target="_blank">GitHub</a></li>
+			<li><a class="first" href="http://www.twitter.com/#!/ppi_framework" target="_blank">Twitter Feed</a></li>
+			<li><a class="first" href="<?= $baseUrl; ?>live-chat" target="_blank">Live Chat</a></li>
+			<li><a class="first" href="" target="_blank">How to get involved?</a></li>
+			<li class="box">
+				<p class="title">PPI IRC Network</p>
+				<p class="details">
+					<strong>Server:</strong> irc.freenode.org<br>
+					<strong>Channel:</strong> #ppi
+				</p>
+				<p><a class="btn success" href="<?= $baseUrl; ?>live-chat" target="_blank">Connect</a></p>
+			</li>
+			<li class="box newsletter-box">
+				<p class="title">PPI Newsletter</p>
+				<div class="form">
+					<p>Email</p> <input id="newsletter-email" name="email" type="text" placeholder="Type email, press enter." /></p>
+					<div class="clear"></div>
+					<input type="submit" class="btn success submit" href="<?= $baseUrl; ?>live-chat" target="_blank" value="Subscribe">
+				</div>
+			</li>
+		</ul>
+		</nav>
+	</div>
+	<div class="content-box">
+
+		<ul class="breadcrumb">
+			<li><a href="#">Home</a> <span class="divider">/</span></li>
+			<li class="active">Contribute</li>
+		</ul>
+
+		<h1>Contributing to PPI</h1>
+
+		<div class="github">
+			<p>The easiest way to contribute to ppi is to fork us on GitHub.</p>
+			<p>1.- Go to <a href="http://github.com/ppi/framework">our github's url</a> and click on the fork button:</p>
+			<img src="<?=$baseUrl;?>images/github-fork.png" />
+			<p>2.- Then clone your repo to your computer:</p>
+			<pre><code>git clone git://github.com/YOURUSER/framework.git</code></pre>
+		</div>
+
+	</div>
+	<div class="clear"></div>
+</div>
