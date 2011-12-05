653d86916d1ff813d7e81af62ef5e2d21930024c
Paul Dragoonis <dragoonis@gmail.com>
2011-12-05 03:18:47 +0000

    Fixing bug #12 - adding href to community page feed elems

--- App/View/default/community/index.php
+++ App/View/default/community/index.php
@@ -19,13 +19,14 @@
 			<?php
 			foreach($activity as $item):
 				$feedImage  = $baseUrl . 'images/community/' . $item['source'] . '.png';
+				$feedDesc = $helper->escape($item['title']);
 			?>
 				<div class="feed source-<?= $item['source']; ?>">
 					<div class="image">
 						<img class="fl <?= $item['source']; ?>" src="<?= $feedImage; ?>" alt="<?= $item['source']; ?>" />
 					</div>
 					<div class="content">
-						<div class="description"><?= $item['title']; ?></div>
+						<div class="description"><a href="<?= $item['url']; ?>" title="$feedDesc"><?= $feedDesc; ?></a></div>
 
 						<div class="actions">
 							<a href="<?= $item["url"];?>" target="_blank" class="readmore">Learn More...</a>
