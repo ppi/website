01ff93410057307254382eeb6dac650e9b113d14
Paul Dragoonis <dragoonis@gmail.com>
2011-12-05 03:24:10 +0000

    Including dynamic leftnav on contribute page

--- App/View/default/community/contribute.php
+++ App/View/default/community/contribute.php
@@ -1,30 +1,6 @@
 <div class="contribute-page">
 	<div class="left-side">
-		<nav>
-		<ul>
-			<li><a class="first" href="<?= $baseUrl; ?>contributors" target="_blank">Contributors</a></li>
-			<li><a class="first" href="http://www.github.com/ppi" target="_blank">GitHub</a></li>
-			<li><a class="first" href="http://www.twitter.com/#!/ppi_framework" target="_blank">Twitter Feed</a></li>
-			<li><a class="first" href="<?= $baseUrl; ?>live-chat" target="_blank">Live Chat</a></li>
-			<li><a class="first" href="" target="_blank">How to get involved?</a></li>
-			<li class="box">
-				<p class="title">PPI IRC Network</p>
-				<p class="details">
-					<strong>Server:</strong> irc.freenode.org<br>
-					<strong>Channel:</strong> #ppi
-				</p>
-				<p><a class="btn success" href="<?= $baseUrl; ?>live-chat" target="_blank">Connect</a></p>
-			</li>
-			<li class="box newsletter-box">
-				<p class="title">PPI Newsletter</p>
-				<div class="form">
-					<p>Email</p> <input id="newsletter-email" name="email" type="text" placeholder="Type email, press enter." /></p>
-					<div class="clear"></div>
-					<input type="submit" class="btn success submit" href="<?= $baseUrl; ?>live-chat" target="_blank" value="Subscribe">
-				</div>
-			</li>
-		</ul>
-		</nav>
+	<?= include($viewDir . 'community/elements/leftnav.php'); ?>
 	</div>
 	<div class="content-box">
 
