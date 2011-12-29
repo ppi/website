662cc14db9e2bbea8e150b7491f3bc24469cf8f2
Paul Dragoonis <dragoonis@gmail.com>
2011-12-29 23:08:56 +0000

    Cleaning up the API parsing code, moving it into the App\Data\Api folder.

--- App/Controller/Api.php
+++ App/Controller/Api.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Controller;
+use App\Data\ApiParser;
 class Api extends Application {
 	
 	function preDispatch() {
@@ -10,37 +11,28 @@ class Api extends Application {
 	 * List the entire project API.
 	 */
 	function index() {
-		
+
 		$files = glob(ROOTPATH . 'api' . DS . '*.xml');
 		foreach($files as $file) {
 			$pathinfo = pathinfo($file);
 			$filenames[] = str_replace('.xml', '', $pathinfo['filename']);
 		}
-		
 		$this->render('api/index', compact('filenames'));
 	}
 	
 	function show() {
-		
+
 		$file      = $this->get('show', '');
 		$cache     = $this->getCache();
 		$cacheName = 'api__' . $file;
 		if($cache->exists($cacheName)) {
 			$apiObject = $cache->get($cacheName);
 		} else {
-			$apiObject = $this->getAPIObject($this->getXMLFromFile($file));
+			$apiParser = new ApiParser();
+			$apiObject = $apiParser->parseDocBloxXML($apiParser->getXMLFromFile($file));
+			$cache->set($cacheName, $apiObject);
 		}
-		
 		$this->render('api/show', compact('apiObject'));
-	}
-	
-	protected function getXMLFromFile($file) {
-
-		$filename = ROOTPATH . 'api' . DS . $file . '.xml';
-		if(file_exists($filename)) {
-			return file_get_contents($filename);
-		}
-		return '';
 	}
 	
 	/**
@@ -52,70 +44,7 @@ class Api extends Application {
 	 */
 	protected function getAPIObject($xml) {
 
-		$apiObject = new \App\Data\ApiObject();
-		$class = simplexml_load_string($xml);
 
-		// Class Options
-		$apiObject->set('name', ltrim( (string) $class->full_name, '\\'));
-		$apiObject->set('final', (string) $class['final'] == 'true');
-		$apiObject->set('abstract', (string) $class['abstract'] == 'true');
-
-		// Properties
-		foreach($class->property as $property) {
-			$prop = new \App\Data\Api\Property();
-			$prop->set('name', (string) $property->name);
-			$prop->set('visibility', (string) $property->visibility);
-			$prop->set('final', (string) $property['final'] == 'true');
-			$prop->set('static', (string) $property['static'] == 'true');
-			$prop->set('line', (string) $property['line']);
-			$apiObject->addProperty($prop);
-		}
-		
-		// Methods
-		foreach($class->method as $method) {
-
-			$m = new \App\Data\Api\Method();
-			$m->set('name', (string) $method->name);
-			$m->set('final', (string) $method['final'] == 'true');
-			$m->set('static', (string) $method['static'] == 'true');
-			$m->set('visibility', (string) $method['visibility']);
-			$m->set('abstract', (string) $method['abstract'] == 'true');
-			$m->set('line', (string) $method['line']);
-			
-			if(isset($method->docblock)) {
-	
-				$m->set('description', (string) $method->docblock->description);
-
-				if(isset($method->docblock->tag)) {
-
-					foreach($method->docblock->tag as $tag) {
-						
-						if($tag['name'] == 'param') {
-							
-							$m->addArgument(array(
-								'name' => (string) $tag['variable'],
-								'type' => (string) $tag['type'],
-								'desc' => (string) $tag['description'],
-								'line' => (string) $tag['line']
-							));
-							
-						} elseif($tag['name'] == 'return') {
-							
-							$m->addReturnValue(array(
-								'type' => (string) $tag['type'],
-								'line' => (string) $tag['line']
-							));
-						}
-					}
-				}
-				
-			}
-
-			
-			$apiObject->addMethod($m);
-		}
-		
-		return $apiObject;
 
 	}
 	
