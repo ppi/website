b1008582b0de3284fda515e7683be599c376f3b2
Paul Dragoonis <dragoonis@gmail.com>
2012-07-04 19:26:10 +0100

    Orientation fix

--- scripts/docs.js
+++ scripts/docs.js
@@ -14,7 +14,11 @@ jQuery(document).ready(function($) {
     });
 
     $(window).bind( 'orientationchange', function(e){
-       alert(e.orientation);
+        if(e.orientation === 'portrait') {
+//            $('.toc-mobile').effects('shake', {times: 5}, 3000);
+            $('.toc-mobile').show();
+            $('.toc-container .toc').hide();
+        }
     });
 
 
