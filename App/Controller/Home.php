93625c07fa5546b6e072b409de1fd229ba4278f9
Paul Dragoonis <dragoonis@gmail.com>
2012-07-13 03:28:52 +0100

    Adding a new dropdown menu to the download button on the homepage.

--- App/Controller/Home.php
+++ App/Controller/Home.php
@@ -8,7 +8,7 @@ class Home extends Application {
 	function index() {
 		$downloadCount = $this->getDownloadCount();
 		$this->addCSS('light/home');
-		$this->addJS('home');
+		$this->addJS('bootstrap/dropdown', 'home');
 		$this->render('home/index', compact('downloadCount'));
 	}
 
--- App/View/default/home/index.php
+++ App/View/default/home/index.php
@@ -6,9 +6,15 @@
 			<p class="heading">ppi framework version 2 is here!</p>
 			<p class="desc">ppi is an open source php meta-framework. we have taken the good bits from Symfony2 and ZendFramework2 and combined them together to create a solid and easy web application framework. If you are already a fan of SF2 or ZF2 then you're already familiar with ppi. ppi is fully <a href="http://www.php-fig.org" title="PSR-0" target="_blank">PSR-0</a>, <a href="http://www.php-fig.org" title="PSR-0" target="_blank">PSR-1</a> and <a href="http://www.php-fig.org" title="PSR-0" target="_blank">PSR-2</a> complaint.</p>
 			<div class="buttons">
-				<a class="btn btn-success btn-large" href="#download" id="download-button"><i class="icon-circle-arrow-down icon-white"></i> Download</a>
-				<a class="btn btn-large" href="<?= $baseUrl; ?>docs">Documentation</a>
-				<a class="btn btn-large" href="http://www.github.com/ppi">GitHub</a>
+				<div class="btn-group">
+					<a class="btn btn-success btn-large dropdown-toggle" data-toggle="dropdown" href="#"><i class="icon-circle-arrow-down icon-white"></i> Download</a>
+					<ul class="dropdown-menu">
+						<li><a href="#download" id="download-skeleton-without-vendors">Skeleton App (without vendors) .tar.gz 5.8KB</a></li>
+						<li><a href="#download" id="download-skeleton-with-vendors">Skeleton App (with vendors) .tar.gz 20MB</a></li>
+					</ul>
+				</div>
+				<a class="main-button docs-button btn btn-large" href="<?= $baseUrl; ?>docs">Documentation</a>
+				<a class="main-button github-button btn btn-large" href="http://www.github.com/ppi">GitHub</a>
 			</div>
 		</div>
 	</div>
