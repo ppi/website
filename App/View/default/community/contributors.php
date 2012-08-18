d1f332e762ffa66d6100c73c0d63172b9eb88924
Paul Dragoonis <dragoonis@gmail.com>
2012-08-18 13:50:20 +0100

    Creating the ppi2 contributors page

--- App/View/default/community/contributors.php
+++ App/View/default/community/contributors.php
@@ -1,102 +1,74 @@
 <div class="contributors-page">
-	<div class="left-side">
-	<?php include_once($viewDir . 'community/elements/leftnav.php'); ?>
-	</div>
-	<div class="content-box">
-		
-		<ul class="breadcrumb">
-			<li><a href="#">Home</a> <span class="divider">/</span></li>
-			<li class="active">Contributors</li>
-		</ul>
-		
-		<h1>Contributors</h1>
+    <div class="left-side">
+    <?php include_once($viewDir . 'community/elements/leftnav.php'); ?>
+    </div>
+    <div class="content-box">
+        
+        <ul class="breadcrumb">
+            <li><a href="#">Home</a> <span class="divider">/</span></li>
+            <li class="active">Contributors</li>
+        </ul>
+        
+        <h1>Contributors</h1>
+        
+        <div class="projects-list">
+        
+            <p>The following people are considered core developers of ppi2. Feel free to reach out to them for questions or support.</p>
+            <div class="project">
+                <div class="title">PPI Project</div>
+                 <p>Paul Dragoonis - Twitter: <a target="_blank" href="http://twitter.com/#!/dr4goonis" title="twitter">@dr4goonis</a> - Email: paul[at]ppi.io</p>
+                 <p>Vítor Brandão - Twitter: <a target="_blank" href="http://twitter.com/#!/noiselabs" title="twitter">@noiselabs</a> - Email: vitor[at]ppi.io</p>
+            </div>
+            
+            <p class="intro">As ppi2 is constructed of components from existing libraries, we directly reached out to the core developers of those projects for assistance and advice on how to get the best out of their library. In addition to this, several members of the open source community have really helped too. The following people have been extremely influential in the creation process of ppi2. Viva la PHP community!</p>
 
-		<div class="projects-list">
-			<div class="project">
-				<div class="title">Advisory Team</div>
-				<div class="description">The Wise Monks - Helping direct the future of PPI</div>
-				<p>Cal Evans (cal@calevans.com) - <a target="_blank" href="http://twitter.com/#!/CalEvans" title="twitter">Twitter</a> - <a  target="_blank" href="https://github.com/calevans" title="github">Github</a> - <a href="http://blog.calevans.com/" title="website">Website</a></p>
-				<p>Davey Shafik (davey@php.net) - <a target="_blank" href="http://twitter.com/#!/dshafik" title="twitter">Twitter</a> - <a target="_blank" href="https://github.com/dshafik" title="github">Github</a></p>
-				<p>David Rogers (david@orlandophp.org) - <a target="_blank" href="http://twitter.com/#!/al_the_x" title="twitter">Twitter</a> - <a target="_blank" href="https://github.com/al-the-x" title="github">Github</a> - <a target="_blank" href="http://ethos-development.com" title="website">Website</a></p>
-				<p>Guilherme Blanco (guilhermeblanco@php.net) - <a target="_blank" href="http://twitter.com/#!/guilhermeblanco" title="twitter">Twitter</a> - <a target="_blank" href="https://github.com/guilhermeblanco" title="github">Github</a> - <a href="http://blog.bisna.com/" title="website">Website</a></p>
-				<p>John Mertic (jmertic@php.net) - <a target="_blank" href="http://twitter.com/#!/jmertic" title="twitter">Twitter</a> - <a target="_blank" href="https://github.com/jmertic" title="github">Github</a> - <a target="_blank" href="http://jmertic.wordpress.com/" title="website">Website</a></p>
-				<p>Juozas Kaziukėnas (juozas@juokaz.com) - <a target="_blank" href="http://twitter.com/#!/juokaz" title="twitter">Twitter</a> - <a target="_blank" href="https://github.com/juokaz" title="github">Github</a> - <a target="_blank" href="http://juokaz.com" title="website">Website</a></p>
-				<p>Mark Schaschke (mark@fractalturtle.com) - <a target="_blank" href="http://twitter.com/#!/epitomus" title="twitter">Twitter</a> - <a target="_blank" href="https://github.com/epitomus" title="github">Github</a> - <a target="_blank" href="http://groupspaces.com/" title="website">Website</a></p>
-				<p>Ryan Mauger (ryan@rmauger.co.uk) - <a target="_blank" href="http://twitter.com/#!/bittarman" title="twitter">Twitter</a> - <a target="_blank" href="http://twitter.com/#!/bittarman" title="github">Github</a> - <a target="_blank" href="http://www.rmauger.co.uk/" title="website">Website</a></p>
-			</div>
-			
-			<div class="project">
-				<div class="title">PPI Framework</div>
-				<div class="description">The Framework Guys</div>
-					<p>Paul Dragoonis (dragoonis@php.net) - <a target="_blank" href="http://twitter.com/dr4goonis" title="twitter">Twitter</a> - <a target="_blank" href="http://github.com/dragoonis" title="github">Github</a> - <a target="_blank" href="http://www.ppiframework.com" title="website">Website</a></p>
-					<p>Anthony Van de Gejuchte (anthonyvdg@hotmail.com) - <a target="_blank" href="http://twitter.com/avdg" title="twitter">Twitter</a> - <a target="_blank" href="http://github.com/avdg" title="github">Github</a></p>
-					<p>Alexey Shein (confik@gmail.com) - <a target="_blank" href="http://twitter.com/#!/shein_alexey" title="twitter">Twitter</a> - <a target="_blank" href="http://github.com/conf" title="github">Github</a></p>
-					<p>Yousif Masoud (yousif.masoud@gmail.com) - <a target="_blank" href="http://twitter.com/#!/ymasoud" title="twitter">Twitter</a> - <a target="_blank" href="https://github.com/YMas" title="github">Github</a></p>
-					<p>Cesar Rodas (crodas@php.net) - <a target="_blank" href="http://twitter.com/#!/crodas" title="twitter">Twitter</a> - <a target="_blank" href="https://github.com/crodas" title="github">Github</a> - <a target="_blank" href="http://cesarrodas.com" title="website">Website</a></p>
-					<p>Alfredo Juarez (alfredojv@juvasoft.com) - <a target="_blank" href="http://twitter.com/alfrekjv" title="twitter">Twitter</a> - <a target="_blank" href="http://github.com/alfrekjv" title="github">Github</a> - <a target="_blank" href="http://juvasoft.com" title="website">Website</a></p>
-					<p>Robert Eisele (crypt@php.net) - <a target="_blank" href="http://twitter.com/#!/RobertEisele" title="twitter">Twitter</a> - <a target="_blank" href="https://github.com/infusion" title="github">Github</a> - <a target="_blank" href="http://www.xarg.org" title="website">Website</a></p>
-					<p>Daniel Egeberg (degeberg@php.net)</p>
-					<p>Johnny Mast (mastjohnny@gmail.com)</p>
-					<p>Alexandre Gaigalas (alexandre@gaigalas.net) - <a href="http://twitter.com/#!/alganet" title="Twitter" target="_blank">Twitter</a> - <a target="_blank" title="Github" href="http://github.com/alganet">Github</a> - <a target="_blank" href="http://about.me/alganet" title="about.me">Website</a></p>
-					<p>Ben Tadiar (ben@handcraftedbyben.co.uk) - <a href="http://twitter.com/#!/benthedesigner" title="Twitter" target="_blank">Twitter</a> - <a target="_blank" title="Github" href="http://github.com/benthedesigner">Github</a></p>
-			</div>
-			
-			<div class="project">
-				<div class="title">PPI Skeleton Application</div>
-				<div class="description">The guys making PPI into a RAD platform.</div>
-				<p>Paul Dragoonis (dragoonis@php.net) - <a target="_blank" href="http://twitter.com/dr4goonis" title="twitter">Twitter</a> - <a target="_blank" href="http://github.com/dragoonis" title="github">Github</a> - <a target="_blank" href="http://www.ppiframework.com" title="website">Website</a></p>
-				<p>Ross Moroney (code.ph0y@googlemail.com) - <a target="_blank" href="https://github.com/rossmoroney" title="github">Github</a></p>
-				<p>Ryan Cummins (ryan@zenmedia.co.uk) - <a target="_blank" href="https://github.com/zenmedia" title="github">Github</a> - <a target="_blank" href="http://www.zenmedia.co.uk" title="website">Website</a></p>
-			</div>
-			
-			<div class="project">
-				<div class="title">PPI Website</div>
-				<div class="description">The guys behind the PPI website.</div>
-					<p>Paul Dragoonis (dragoonis@php.net) - <a target="_blank" href="http://twitter.com/dr4goonis" title="twitter">Twitter</a> - <a target="_blank" href="http://github.com/dragoonis" title="github">Github</a> - <a target="_blank" href="http://www.ppiframework.com" title="website">Website</a></p>
-					<p>Alfredo Juarez (alfredojv@juvasoft.com) - <a target="_blank" href="http://twitter.com/alfrekjv" title="twitter">Twitter</a> - <a target="_blank" href="http://github.com/alfrekjv" title="github">Github</a> - <a target="_blank" href="http://juvasoft.com" title="website">Website</a></p>
-					<p>Ben Tadiar (me@bentadiar.co.uk) - <a target="_blank" href="http://twitter.com/#!/BenTheDesigner" title="twitter">Twitter</a> - <a target="_blank" href="https://github.com/BenTheDesigner" title="github">Github</a> - <a target="_blank" href="www.bentadiar.co.uk" title="website">Website</a></p>
-					<p>Ricky Stevens (ppi@ricky-stevens.com) - <a target="_blank" href="http://ricky-stevens.com/" title="website">Website</a></p>
-					<p>Eric Couturier-Guillaume (eric@eric-cg.fr) - <a target="_blank" href="http://twitter.com/#!/eric2145" title="twitter">Twitter</a> - <a target="_blank" href="https://github.com/Eric-CG" title="github">Github</a> - <a target="_blank" href="http://www.sydec-group.com" title="website">Website</a></p>
-			</div>
-			
-			<div class="project">
-				<div class="title">PPI Issue Tracker</div>
-				<div class="description">The issue tracker for the PPI project</div>
-				<p>Paul Dragoonis (dragoonis@php.net) - <a target="_blank" href="http://twitter.com/dr4goonis" title="twitter">Twitter</a> - <a target="_blank" href="http://github.com/dragoonis" title="github">Github</a> - <a target="_blank" href="http://www.ppiframework.com" title="website">Website</a></p>
-				<p>Ross Moroney (code.ph0y@googlemail.com) - <a target="_blank" href="https://github.com/rossmoroney" title="github">Github</a></p>
-				<p>Eric Couturier-Guillaume (eric@eric-cg.fr) - <a target="_blank" href="http://twitter.com/#!/eric2145" title="twitter">Twitter</a> - <a target="_blank" href="https://github.com/Eric-CG" title="github">Github</a> - <a target="_blank" href="http://www.sydec-group.com" title="website">Website</a></p>
-				<p>Alfredo Juarez (alfredojv@juvasoft.com) - <a target="_blank" href="http://twitter.com/alfrekjv" title="twitter">Twitter</a> - <a target="_blank" href="http://github.com/alfrekjv" title="github">Github</a> - <a target="_blank" href="http://juvasoft.com" title="website">Website</a></p>
-			</div>
-			
-			<div class="project">
-				<div class="title">PPI Documentation</div>
-				<div class="description">The guys helping make documentation possible</div>
-				<p>Paul Dragoonis (dragoonis@php.net) - <a target="_blank" href="http://twitter.com/dr4goonis" title="twitter">Twitter</a> - <a target="_blank" href="http://github.com/dragoonis" title="github">Github</a> - <a target="_blank" href="http://www.ppiframework.com" title="website">Website</a></p>
-			</div>
-			
-			<div class="project">
-				<div class="title">API Documentation</div>
-				<div class="description">The guys giving you API documentation</div>
-				<p>Paul Dragoonis (dragoonis@php.net) - <a target="_blank" href="http://twitter.com/dr4goonis" title="twitter">Twitter</a> - <a target="_blank" href="http://github.com/dragoonis" title="github">Github</a> - <a target="_blank" href="http://www.ppiframework.com" title="website">Website</a></p>
-				<p>Mike Van Riel (mike.vanriel@naenius.com) - <a target="_blank" href="http://twitter.com/#!/mvriel" title="twitter">Twitter</a> - <a target="_blank" href="https://github.com/mvriel" title="github">Github</a> - <a target="_blank" href="http://blog.naenius.com" title="website">Website</a></p>
-			</div>
-			
-			<div class="project">
-				<div class="title">Community</div>
-				<div class="description">The growing list of community helpers</div>
-				<p>Ashley Dragoonis (ashleydragoonis@gmail.com) - <a target="_blank" href="http://twitter.com/#!/AshleyDragoonis" title="twitter">Twitter</a> - <a target="_blank" href="https://github.com/AshleyDragoonis" title="github">Github</a>
-			</div>
-			
-			<div class="project">
-				<div class="title">Miscellaneous</div>
-				<div class="description">People who deserve attribution</div>
-				<p>Ryan Cummins (ryan@zenmedia.co.uk) - <a target="_blank" href="https://github.com/zenmedia" title="github">Github</a> - <a target="_blank" href="http://www.zenmedia.co.uk" title="website">Website>Website</a></p>
-				<p>Anthony Fererra (ircmaxell@php.net) - <a target="_blank" href="http://twitter.com/#!/ircmaxell" title="twitter">Twitter</a> - <a target="_blank" href="https://github.com/ircmaxell" title="github">Github</a> - <a target="_blank" href="http://blog.ircmaxell.com/" title="website">Website</a></p>
-				<p>Mark Roduner (mark.roduner@gmail.com) - <a target="_blank" href="https://github.com/mroduner" title="github">Github</a></p>
-			</div>
-			
-		</div>
-
-	</div>
-	<div class="clear"></div>
+            <div class="project">
+                <div class="title">Symfony Project</div>
+                <p>Christophe Coevoet (<a target="_blank" href="http://twitter.com/#!/Stof70" title="twitter">@Stof70</a>)</p>
+                <p>Lukas Kahwe Smith (<a target="_blank" href="http://twitter.com/#!/lsmith" title="twitter">@lsmith</a>)</p>
+                <p>Kris Wallsmith (<a target="_blank" href="http://twitter.com/#!/kriswallsmith" title="twitter">@kriswallsmith</a>)</p>
+            </div>
+            
+            <div class="project">
+                <div class="title">Zend Framework</div>
+                <p>Evan Coury (<a target="_blank" href="http://twitter.com/#!/EvanDotPro" title="twitter">@EvanDotPro</a>)</p>
+                <p>Marco Pivetta (<a target="_blank" href="http://twitter.com/#!/Ocramius" title="twitter">@Ocramius</a>)</p>
+                <p>Ralph Schindler (<a target="_blank" href="http://twitter.com/#!/ralphschindler" title="twitter">@ralphschindler</a>)</p>
+                <p>Matthew Weier O'Phinney (<a target="_blank" href="http://twitter.com/#!/weierophinney" title="twitter">@weierophinney</a>)</p>
+            </div>
+            
+            <div class="project">
+                <div class="title">Composer Project</div>
+                <p>Jordi Boggiano  (<a target="_blank" href="http://twitter.com/#!/Seldaek" title="twitter">@Seldaek</a>)</p>
+                <p>Nils Adermann (<a target="_blank" href="http://twitter.com/#!/naderman" title="twitter">@naderman</a>)</p>
+            </div>
+            
+            <div class="project">
+                <div class="title">Doctrine Project</div>
+                <p>Guilherme Blanco (<a target="_blank" href="http://twitter.com/#!/guilhermeblanco" title="twitter">@guilhermeblanco</a>)</p>
+            </div>
+            
+            <div class="project">
+                <div class="title">PHP Community Members</div>
+                <p>Beau D. Simensen (<a target="_blank" href="http://twitter.com/#!/kirkryyn" title="twitter">@kirkryyn</a>)</p>
+                <p>Stefan Koopmanschap  (<a target="_blank" href="http://twitter.com/#!/skoop" title="twitter">@skoop</a>)</p>
+            </div>
+            
+            <p>Special thanks to these guys who are early adopters of ppi2, the project's you've made on ppi2 have helped us solidify the core framework and make a feature-rich framework stack.</p>
+            
+            <div class="project">
+                <div class="title">Early Adopters</div>
+                <p>Sean Warren Koole (<a target="_blank" href="http://twitter.com/#!/seankoole" title="twitter">@seankoole</a>)</p>
+            </div>
+            
+            <p>Our hosting services have been donated by ZenMedia. Special thanks goes out to them.</p>
+            
+            <div class="project">
+                <div class="title">Donators</div>
+                <p>Ryan Cummins - Website: <a target="_blank" href="http://www.zenmedia.co.uk" title="website">www.zenmedia.co.uk</a></p>
+            </div>
+            
+       </div> <!-- /.projects-list -->
+    </div>
+    <div class="clear"></div>
 </div>
