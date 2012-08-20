b4e719360c9768a9da926bdb20b99c1a4f486125
Paul Dragoonis <dragoonis@gmail.com>
2012-08-20 11:31:43 +0100

    Updating footer links

--- App/View/default/elements/footer.php
+++ App/View/default/elements/footer.php
@@ -4,12 +4,9 @@
 		<div class="inside">
 			<div class="logo"><img src="<?= $baseUrl; ?>images/light/ppi-white.png" alt="Logo"></div>
 			<div class="content links">
-				<a href="#" title="">Get Started</a>
-				<a href="#" title="">Documentation</a>
-				<a href="#" title="">Community</a>
+				<a href="<?=$baseUrl;?>docs" title="Documentation">Documentation</a>
+				<a href="<?=$baseUrl;?>community" title="Community">Community</a>
 				<a href="<?= $baseUrl; ?>projects" title="">Projects</a>
-				<a href="#" title="">Blog</a>
-				
 				<p class="desc">PPI is an Open Source Project under the MIT license.</p>
 			</div>
 			<div class="clear"></div>
