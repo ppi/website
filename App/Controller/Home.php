7168d1039b1d3c1a56f4be32d45e86ede87092c2
Paul Dragoonis <dragoonis@gmail.com>
2012-08-20 15:12:04 +0100

    Fixing homepage downoad button from being broken

--- App/Controller/Home.php
+++ App/Controller/Home.php
@@ -8,7 +8,7 @@ class Home extends Application {
 	function index() {
 		$downloadCount = $this->getDownloadCount();
 		$this->addCSS('light/home');
-		$this->addJS('bootstrap/dropdown', 'home');
+		$this->addJS('home');
 		$this->render('home/index', compact('downloadCount'));
 	}
 
