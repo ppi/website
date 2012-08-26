933227f335912dec41d3524c34a4b3c1d7903e56
Paul Dragoonis <dragoonis@gmail.com>
2012-08-26 15:32:44 +0100

    Footer mods

--- App/View/default/elements/footer.php
+++ App/View/default/elements/footer.php
@@ -4,10 +4,11 @@
 		<div class="inside">
 			<div class="logo"><img src="<?= $baseUrl; ?>images/light/ppi-white.png" alt="Logo"></div>
 			<div class="content links">
-				<a href="<?=$baseUrl;?>docs" title="Documentation">Documentation</a>
-				<a href="<?=$baseUrl;?>community" title="Community">Community</a>
-				<a href="<?= $baseUrl; ?>projects" title="">Projects</a>
-				<p class="desc">PPI is an Open Source Project under the MIT license.</p>
+                <a href="<?=$baseUrl;?>community" title="Community">Community</a>
+                <a href="<?=$baseUrl;?>about" title="About">About</a>
+                <a href="<?= $baseUrl; ?>projects" title="">Projects</a>
+                <a href="<?=$baseUrl;?>docs" title="Documentation">Documentation</a>
+                <p class="desc">PPI is an Open Source Project under the MIT license.</p>
 			</div>
 			<div class="clear"></div>
 		</div>
