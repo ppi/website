a27b1a7127cbbadfb3ba6a38fe75ff340a57dd93
Paul Dragoonis <dragoonis@gmail.com>
2012-09-08 16:04:01 +0100

    re-committing accidental removed app files

---
+++ app/app.config.php
@@ -0,0 +1,15 @@
+<?php
+$config = array(
+    'environment'            => 'development',
+    'templating.engines'     => array('php', 'smarty', 'twig'),
+    'templating.globals'     => array(
+        'ga_tracking' => 'UA-XXXXX-X'),
+    'datasource.connections' => include (__DIR__ . '/datasource.config.php')
+);
+
+// Are we in debug mode ?
+if ($config['environment'] !== 'development') {
+    $config['debug']     = $config['environment'] === 'development';
+    $config['cache_dir'] = __DIR__ . '/cache';
+}
+return $config; // Very important
