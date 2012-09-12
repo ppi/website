3ca37b2e9b762848318b6b1c845d7ae3fe6cd732
Alfredo Juarez <alfrekjv@gmail.com>
2012-09-11 19:32:47 -0600

    Added JS to fetch Recent Posts completing Issue #60

--- public/mod/blog/blog.js
+++ public/mod/blog/blog.js
@@ -1,16 +1,29 @@
 jQuery(document).ready(function ($) {
-	
-	$.get(ppi.baseUrl + 'blog/get_popular_tags', {}, function(response) {
-		
-		var content;
-		if(response.tags.length == 0) {
-			content = '<li><p>No Popular Tags</p></li>';
-		} else {
-			content = Mustache.render($('#popular-tags-template').html(), response);
-		}
 
-		$('#popular-tags-content').html(content);
-		
-	}, 'json');
+    $.get(ppi.baseUrl + 'blog/get_popular_tags', {}, function (response) {
+
+        var content;
+        if (response.tags.length == 0) {
+            content = '<li><p>No Popular Tags</p></li>';
+        } else {
+            content = Mustache.render($('#popular-tags-template').html(), response);
+        }
+
+        $('#popular-tags-content').html(content);
+
+    }, 'json');
+
+    $.get(ppi.baseUrl + 'blog/get_recent_comments', {}, function (response) {
+
+        var content;
+        if (response.comments.length == 0) {
+            content = '<li><p>No Recent Comments</p></li>';
+        } else {
+            content = Mustache.render($('#recent-comments-template').html(), response);
+        }
+
+        $('#recent-comments-content').html(content);
+
+    }, 'json');
 
 });
