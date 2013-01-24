9e2d154f2730feda1e97d1541a43941729f2f00b
Paul Dragoonis <dragoonis@gmail.com>
2013-01-24 03:30:25 +0000

    fixing header includes for the blog

--- app/views/docsbase.html.php
+++ app/views/docsbase.html.php
@@ -43,48 +43,6 @@
 
     <!-- Prompt IE 6 users to install Chrome Frame. Remove this if you support IE 6. -->
     <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
-    
-    <div class="topbar clearfix docs-header" data-dropdown="dropdown" id="header">
-        
-        <div class="topbar-inner">
-            
-            <div class="navbar navbar-fixed-top">
-                
-                <div class="navbar-inner">
-                    
-                    <div class="container">
-                        
-                        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </a>
-                        
-                        <div class="nav-collapse">
-                            <ul class="nav">
-                                <li class="logo-item"><a class="logo" href="<?=$view['router']->generate('Homepage');?>" title="PPI"><img src="<?=$view['assets']->getUrl('images/ppi-white.png');?>" alt="Logo" height="25"></a></li>
-                                <li class=""><a href="<?=$view['router']->generate('Homepage');?>">Home</a></li>
-                                <li><a href="<?=$view['router']->generate('About');?>">About</a></li>
-                                <li class=""><a href="<?=$view['router']->generate('Homepage');?>community">Community</a></li>
-                                <li class=""><a href="<?=$view['router']->generate('Projects');?>">Projects</a></li>
-                                <li class="dropdown docs-dropdown">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Documentation <b class="caret icon-white"></b></a>
-                                    <ul class="dropdown-menu">
-                                        <li><a href="<?=$view['router']->generate('DocsIndex', array('page' => 'getting-started'));?>">Getting Started</a></li>
-                                        <li><a href="<?=$view['router']->generate('DocsIndex', array('page' => 'application'));?>">The Skeleton Application</a></li>
-                                        <li><a href="<?=$view['router']->generate('DocsIndex', array('page' => 'modules'));?>">Modules</a></li>
-                                        <li><a href="<?=$view['router']->generate('DocsIndex', array('page' => 'routing'));?>">Routing</a></li>
-                                        <li><a href="<?=$view['router']->generate('DocsIndex', array('page' => 'controllers'));?>">Controllers</a></li>
-                                        <li><a href="<?=$view['router']->generate('DocsIndex', array('page' => 'templating'));?>">Templating</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 
     <div class="container" id="docspage-content">
         
---
+++ app/views/header.html.php
@@ -0,0 +1,38 @@
+<div class="topbar clearfix" data-dropdown="dropdown" id="header">
+    <div class="topbar-inner">
+        <div class="navbar navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container">
+                    
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+                    
+                    <div class="nav-collapse">
+                        <ul class="nav">
+                            <li class="logo-item"><a class="logo" href="<?=$view['router']->generate('Homepage');?>" title="PPI"><img src="<?=$view['assets']->getUrl('images/ppi-white.png');?>" alt="Logo" height="25"></a></li>
+                            <li class=""><a href="<?=$view['router']->generate('Homepage');?>">Home</a></li>
+                            <li><a href="<?=$view['router']->generate('About');?>">About</a></li>
+                            <li class=""><a href="<?=$view['router']->generate('Blog');?>">Blog</a></li>
+                            <li class=""><a href="<?=$view['router']->generate('Community');?>">Community</a></li>
+                            <li class=""><a href="<?=$view['router']->generate('Projects');?>">Projects</a></li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Documentation <b class="caret icon-white"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="<?=$view['router']->generate('DocsIndex', array('page' => 'getting-started'));?>">Getting Started</a></li>
+                                    <li><a href="<?=$view['router']->generate('DocsIndex', array('page' => 'application'));?>">The Skeleton Application</a></li>
+                                    <li><a href="<?=$view['router']->generate('DocsIndex', array('page' => 'modules'));?>">Modules</a></li>
+                                    <li><a href="<?=$view['router']->generate('DocsIndex', array('page' => 'routing'));?>">Routing</a></li>
+                                    <li><a href="<?=$view['router']->generate('DocsIndex', array('page' => 'controllers'));?>">Controllers</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
