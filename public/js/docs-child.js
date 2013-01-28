56db51e403e2fe6559daed0ddb5e2276ac4b1aba
Paul Dragoonis <dragoonis@gmail.com>
2013-01-28 01:15:59 +0000

    Updating optional prev/next linkage

--- public/js/docs-child.js
+++ public/js/docs-child.js
@@ -6,37 +6,39 @@ jQuery(document).ready(function ($) {
 	var prev = {
 		link:  ppi.baseUrl + 'docs/' + actionContent.data('prevlink').replace('.php', ''),
 		title: actionContent.data('prevpage')
-	 };
+	};
 	
 	var next = {
-		link:  ppi.baseUrl + 'docs/' + actionContent.data('nextlink').replace('.php', ''),
+		link:  ppi.baseUrl + 'docs/' + actionContent.data('nextlink'),
 		title: actionContent.data('nextpage')
-	 };
+	};
 	
 	h1.find('a').remove();
-	 
+	
 	var newhtml = '<div class="section-subbar clearfix">';
-	 if(prev.link !== undefined) {
-		 newhtml += 
-		 '<a class="prev-page btn" title="Getting Started" href="' + prev.link + '">' +
-				 '<img src="../../images/docs/previous-page.png" alt="Previous">&nbsp;&nbsp;' + prev.title + 
-		 '</a>';
-	 }
+	if(prev.link !== undefined) {
+		prev.link = prev.link.replace('.php', '');
+		newhtml += 
+		'<a class="prev-page btn" title="' + prev.link + '" href="' + prev.link + '">' +
+				'<img src="../../images/docs/previous-page.png" alt="Previous">&nbsp;&nbsp;' + prev.title + 
+		'</a>';
+	}
 	
 	newhtml += '<h1>' + h1.html() + '</h1>';
 	
 	if(next.link !== undefined) {
-	newhtml += 
-	'<a class="next-page btn" title="Modules" href="' + next.link + '">' +
-		   '' + next.title + '&nbsp;&nbsp;<img src="../../images/docs/next-page.png" alt="Next">' +
-	'</a>';
+		next.link = next.link.replace('.php', '');
+		newhtml += 
+		'<a class="next-page btn" title="' + next.link + '" href="' + next.link + '">' +
+			  '' + next.title + '&nbsp;&nbsp;<img src="../../images/docs/next-page.png" alt="Next">' +
+		'</a>';
 	}
 	
 	newhtml += '</div>';
 	
-	 $(newhtml).insertAfter(h1);
-	 
-	 h1.hide();
+	$(newhtml).insertAfter(h1);
+	
+	h1.hide();
 	
 });
 
