d67d765a2834381091520062e4f7e7e1e893fa7f
Paul Dragoonis <dragoonis@gmail.com>
2013-06-15 19:34:57 +0100

    adding downloads sql files

---
+++ modules/Application/resources/sql/download_entry.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `download_entry` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ip_address` varchar(255) DEFAULT NULL,
+  `created` int(15) DEFAULT NULL,
+  `download_item_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `idx_dl_item` (`download_item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
