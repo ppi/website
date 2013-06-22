9f18bb9140d0ad8f5e1f6afa80e9b63189eacb39
Paul Dragoonis <dragoonis@gmail.com>
2013-06-22 13:32:34 +0100

    Updating PPI description

--- modules/Application/resources/views/index/index.html.php
+++ modules/Application/resources/views/index/index.html.php
@@ -7,8 +7,8 @@
         <div class="top">
             <div class="intro">
                 <p class="heading">ppi: the php meta framework!</p>
-                <p class="desc">ppi is an open source php meta-framework. we have taken the good bits from Symfony2, ZendFramework2 &amp; Doctrine2 and combined them together to create a solid and very easy web application framework.
-                <p class="desc">ppi can be considered the boilerplate of PHP frameworks</p>
+                <p class="desc">PPI is a vendor agnostic PHP meta framework that provides an equal and open platform to empower PHP developers to pick the best tools from the best PHP frameworks</p>
+                <p class="desc">PPI bootstraps framework components for you from the top frameworks such as ZendFrameworks2, Symfony2, Laravel4, FuelPHP, Doctrine2<p>
     
                 <div class="buttons">
                     <a class="btn btn-green btn-large" href="<?=$view['router']->generate('Downloads');?>">
