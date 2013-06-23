483300455f3201c8d06448da9d17c1c109b4aeb5
Paul Dragoonis <dragoonis@gmail.com>
2013-06-23 12:36:53 +0100

    About page update

--- modules/Application/resources/views/index/about.html.php
+++ modules/Application/resources/views/index/about.html.php
@@ -29,41 +29,21 @@
             
             <div class="desc">
                 <h2>What is PPI?</h2>
-                <p>Conceptually, PPI is a php framework that combines power and simplicity.</p>
-                <p>Technically, PPI is a simple web framework which harnesses the power of Symfony2, ZendFramework2 and Doctrine2.</p>
-            </div>
-            
-            <div class="desc">
-                <h2>Design Goals</h2>
-                <ul>
-                    <li>Minimise the learning curve of using such powerful components.</li>
-                    <li>Do not re-invent the wheel! Choose the best component for the job from an existing library and make it easily accessible for the developer.</li>
-                    <li>Designed to be cloud compatible from the beginning.</li>
-                    <li>To harness the capabilities of larger more complex frameworks and convey them in a more productive and approachable manner.</li>
-                    <li>To give developers coming from frameworks such as CodeIgniter, Kohana, CakePHP a platform to step up and use much more powerful components while retaining the simplistic routes of those frameworks they've came from.</li>
-                </ul>
+                <p>PPI is a vendor agnostic PHP meta framework that provides an equal and open platform to empower PHP developers to pick the best tools from the best PHP frameworks</p>
+                <p>PPI bootstraps framework components for you from the top frameworks such as ZendFrameworks2, Symfony2, Laravel4, FuelPHP, Doctrine2</p>
             </div>
             
             <div class="desc">
                 <h2>Key Aspects</h2>
                 <ul>
-                    <li>Fully PSR compliant. You can use all of your existing compatible PSR code in PPI.</li>
-                    <li>Re-use of time/code invested in learning/using other frameworks.</li>
                     <li>Clean and simple.</li>
                     <li>Minimal and useful (non-intrusive)</li>
                     <li>Well documented.</li>
-                    <li>Pragmatic but feature rich.</li>
+                    <li>Fully PSR compliant. You can use all of your existing compatible PSR code in PPI.</li>
+                    <li>Re-use of time/code invested in learning/using other frameworks since all code is directly reusable.</li>
                 </ul>
             </div>
             
-            <div class="desc">
-                <h2>Advantages</h2>
-                <ul>
-                    <li>If you know Symfony2 or ZendFramework2 you already know what you're doing in PPI. Therefore all the time you've invested in learning these frameworks can be re-used in building PPI apps.</li>
-                    <li>Knowledge you gain from using PPI will be invaluable to you out there in the wild-wild-web. It's teaching you how to use ZendFramework and Symfony and bits of Doctrine at the same time</li>
-                </ul>
-            </div>
-               
         </div>
     </div>
 </div>
