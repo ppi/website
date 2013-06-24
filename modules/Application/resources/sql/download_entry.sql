676773b04af831e4b8bd0b093150907f60690d31
Paul Dragoonis <dragoonis@gmail.com>
2013-06-24 12:40:10 +0100

    Fixing a links controller issue, adding a SQL file for download_entry, adding utils/*

--- modules/Application/resources/sql/download_entry.sql
+++ modules/Application/resources/sql/download_entry.sql
@@ -3,8 +3,7 @@ CREATE TABLE `download_entry` (
   `ip_address` varchar(255) DEFAULT NULL,
   `created` int(15) DEFAULT NULL,
   `download_item_id` int(11) DEFAULT NULL,
+  `with_vendor` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `idx_dl_item` (`download_item_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
---
+++ utils/populate_downloads_2.0.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# file and dir setup
+rm -rf downloads_tmp/2.0;
+rm -f downloads_tmp/ppi-v2.0.zip;
+rm -f downloads_tmp/ppi-v2.0-with-vendor.zip
+mkdir downloads_tmp/2.0 && cd downloads_tmp/2.0;
+
+# pull skeletonapp 2.0 from github
+git clone https://github.com/ppi/skeletonapp.git . && git checkout 2.0 && cd ..
+
+# create archive
+zip -r ppi-v2.0.zip 2.0
+
+# get size of the archive
+size=$(du -b ppi-v2.0.zip | sed 's/\([0-9]*\)\(.*\)/\1/')
+
+# run composer
+cd 2.0
+curl -sS https://getcomposer.org/installer | php
+php composer.phar install && cd ..
+zip -r ppi-v2.0-with-vendor.zip 2.0
+
+# get size of the archive
+vendorsize=$(du -b ppi-v2.0-with-vendor.zip | sed 's/\([0-9]*\)\(.*\)/\1/')
+
+# update database with filesizes
+
+
+
+# move files into production DIR once completed
+cp ppi-v2.0.zip ../../downloads/ppi-v2.0.zip
+cp ppi-v2.0-with-vendor.zip ../../downloads/ppi-v2.0-with-vendor.zip
