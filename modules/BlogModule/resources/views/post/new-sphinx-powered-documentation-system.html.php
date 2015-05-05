edd88529ee91e45c5b1a21522fbd1179131e8945
Paul Dragoonis <paul.dragoonis@kainos.com>
2015-05-05 12:07:56 +0100

    Sphinx docs updates

--- modules/BlogModule/resources/views/post/new-sphinx-powered-documentation-system.html.php
+++ modules/BlogModule/resources/views/post/new-sphinx-powered-documentation-system.html.php
@@ -5,10 +5,10 @@
     
 <h4>What do we have now?</h4>
 <p>
-    We now have a separate 'docs' repo <a href="http://github.com/ppi/docs/tree/2.0" title="http://github.com/ppi/docs/tree/2.0">here</a>. 
-    Each documentation version is on its own unique branch, so now we are actively using the <a href="http://github.com/ppi/docs/tree/2.0" title="2.0" target="_blank">2.0</a> and <a href="http://github.com/ppi/docs/tree/2.1" title="2.1" target="_blank">2.1</a> branches on that repo. 
-    The system itself is powered by <a href="http://sphinx-doc.org/" title="sphinx" target="_blank">sphinx</a> and uses the <a href="http://docutils.sourceforge.net/rst.html" title="http://docutils.sourceforge.net/rst.html" target="_blank">reStructured (reST) syntax</a>, this is great because it renders natively on github.com like markdown, see an example of this <a href="https://github.com/ppi/docs/blob/2.0/en/book/routing.rst" title="here" target="_blank">here</a>.
+    We now have a separate 'docs' repo <a href="http://github.com/ppi/docs/tree/2.1" title="http://github.com/ppi/docs/tree/2.1">here</a>.
+    Each documentation version is on its own unique branch. We are actively using the <a href="http://github.com/ppi/docs/tree/2.1" title="2.1" target="_blank">2.1</a> branch on that repo.
+    The system itself is powered by <a href="http://sphinx-doc.org/" title="sphinx" target="_blank">sphinx</a> and uses the <a href="http://docutils.sourceforge.net/rst.html" title="http://docutils.sourceforge.net/rst.html" target="_blank">reStructured (reST) syntax</a>, this is great because it renders natively on github.com like markdown, see an example of this <a href="https://github.com/ppi/docs/blob/2.1/en/book/routing.rst" title="here" target="_blank">here</a>.
 </p>
     
 <h4>What are the key benefits of this new system?</h4>
-<p>PPI contributors can now contribute to the documentation without caring about html markup, styling or anything to do with the ppi website structure itself, it's merely markdown template files containing content only. We can now maintain the documentation on a per-version basis now, so as we add or change things to version 2.1 it can be maintained separately from 2.0.</p>
+<p>PPI contributors can now contribute to the documentation without caring about html markup, styling or anything to do with the ppi website structure itself, it's merely markdown template files containing content only. We can now maintain the documentation on a per-version basis now, so as we add or change things to version 2.1 it can be maintained separately from 2.1.</p>
