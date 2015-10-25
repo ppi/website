8f6a4b2a03eb3af5167617715260003dfb5890b3
Paul Dragoonis <dragoonis@gmail.com>
2015-10-25 12:44:22 +0000

    Adding homepage

--- app/views/header.html.php
+++ app/views/header.html.php
@@ -4,6 +4,7 @@
             <div class="navbar-inner" style="width: 800px; margin: 0 auto;">
                 <div class="container" style="width: 800px !important; margin-left: 0">
                     <ul class="nav">
+                        <li><a class="main-button" href="/">Download</a></li>
                         <li><a class="main-button" href="<?= $view['router']->generate('Downloads'); ?>">Download</a></li>
                         <li><a class="main-button docs-button" href="http://docs.ppi.io/latest/">Docs</a></li>
                         <li><a class="main-button github-button" href="http://www.github.com/ppi">GitHub</a></li>
