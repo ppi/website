ad45b519e2cbcce53b3e0303bbc9f777dc2a9495
Paul Dragoonis <dragoonis@gmail.com>
2015-10-27 22:31:39 +0000

    Updating calls from loadYamlRoutes to loadSymfonyRoutes

--- modules/Application/Module.php
+++ modules/Application/Module.php
@@ -20,7 +20,7 @@ class Module extends BaseModule
      */
     public function getRoutes()
     {
-        return $this->loadYamlRoutes(__DIR__ . '/resources/config/routes.yml');
+        return $this->loadSymfonyRoutes(__DIR__ . '/resources/config/routes.yml');
     }
 
     /**
@@ -30,7 +30,7 @@ class Module extends BaseModule
      */
     public function getConfig()
     {
-        return $this->loadYamlConfig(__DIR__ . '/resources/config/config.yml');
+        return $this->loadSymfonyRoutes(__DIR__ . '/resources/config/config.yml');
     }
     
     public function getServiceConfig()
