9e614aa98bf5b383e00a665dd7c90f2752558d35
Paul Dragoonis <dragoonis@gmail.com>
2015-10-27 22:19:10 +0000

    Cleaning up redundant code and files

--- modules/Application/resources/sql/download_entry.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `download_entry` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ip_address` varchar(255) DEFAULT NULL,
-  `created` int(15) DEFAULT NULL,
-  `download_item_id` int(11) DEFAULT NULL,
-  `with_vendor` tinyint(1) DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `idx_dl_item` (`download_item_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- modules/Application/resources/views/docs/index.html.php
+++
@@ -1,2 +0,0 @@
-<?php $view->extend('::docsbase.html.php'); ?>
-<?=$view->render('Application:' . $docsFolder . ':' . $page . '.html.php');?>
